90da0dccc7bdc527cadf0214268429213312bf6f
dwiprst13 <dwiprst13@gmail.com>
2025-08-27 12:49:36 +0700

    Initialized project

---
+++ shortener.go
@@ -0,0 +1,20 @@
+package main
+
+import "crypto/rand"
+
+const alphabet = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+func generateCode(n int) (string, error) {
+	if n <= 0 {
+		n = 6
+	}
+	b := make([]byte, n)
+	if _, err := rand.Read(b); err != nil {
+		return "", err
+	}
+	for i := range b {
+		b[i] = alphabet[int(b[i])%len(alphabet)]
+	}
+
+	return string(b), nil
+}
